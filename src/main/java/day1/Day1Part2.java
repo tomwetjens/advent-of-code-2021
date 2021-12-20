27e5868e2f6738d8fe73976c3105d743c337cb7e
Tom Wetjens <tomwetjens@gmail.com>
2021-12-20 11:52:36 +0100

    initial

---
+++ src/main/java/day1/Day1Part2.java
@@ -0,0 +1,30 @@
+package day1;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+public class Day1Part2 {
+
+    public static void main(String[] args) throws Exception {
+        var input = Files.lines(Paths.get(Day1Part2.class.getResource("/day1-input").toURI()))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+
+        var count = 0;
+        Integer prev = null;
+        for (var i = 0; i < input.size() - 2; i++) {
+            var cur = input.get(i) + input.get(i + 1) + input.get(i + 2);
+
+            if (prev != null && cur > prev) {
+                count++;
+            }
+
+            prev = cur;
+        }
+
+        System.out.println(count);
+    }
+
+}
